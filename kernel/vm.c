7bc6fe57c9432d60a89fc2ccacbaee5bedc5dea4
wwj <1670995411@qq.com>
2021-12-12 18:39:35 +0800

    Finish print pgtbl

--- kernel/vm.c
+++ kernel/vm.c
@@ -439,3 +439,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// Recursively print pagetable info
+void
+vmprint(pagetable_t pagetable, int level)
+{
+  if (level > 2) return;
+
+  if (level == 0) {
+    printf("page table %p\n", pagetable);
+  }
+  
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    // if is valid
+    if(pte & PTE_V){
+      for (int j = 0; j < level; j++) {
+        printf(".. ");
+      }
+      uint64 child = PTE2PA(pte);
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      vmprint((pagetable_t)child, level+1);
+    }
+  }
+}
