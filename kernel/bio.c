eb67f3224f8bd3b0be67f56f13ca43ad6b5b3550
wwj <1670995411@qq.com>
2022-01-17 17:43:50 +0800

    Finish Buffer cache

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,32 +23,34 @@
 #include "fs.h"
 #include "buf.h"
 
+// use hash table instead of bidirectional double linked list
+#define NBFSIZE 5 // if too small, may panic in "usertests manywrites"
+#define NBUCKET 13
+
 struct {
   struct spinlock lock;
-  struct buf buf[NBUF];
+  struct buf buf[NBFSIZE];
+} bcache[NBUCKET];
 
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
-  struct buf head;
-} bcache;
+uint
+hash(uint blockno)
+{
+  return blockno % NBUCKET; // simple linear hash
+}
 
 void
 binit(void)
 {
-  struct buf *b;
+  struct buf *b, *bbuf;
 
-  initlock(&bcache.lock, "bcache");
-
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+  for (int i = 0; i < NBUCKET; ++i) {
+    initlock(&bcache[i].lock, "bcache");
+    bbuf = bcache[i].buf;
+    for (b = bbuf; b < bbuf + NBFSIZE; ++b) { // each buf in the hash bucket
+      initsleeplock(&b->lock, "buffer");
+      b->ticks = ticks; // timestamp
+      b->bucketno = i;  // belong to which bucket
+    }
   }
 }
 
@@ -58,33 +60,51 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
+  uint key = hash(blockno);
   struct buf *b;
 
-  acquire(&bcache.lock);
+  acquire(&bcache[key].lock);
 
+  struct buf *bbuf = bcache[key].buf;
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
-    if(b->dev == dev && b->blockno == blockno){
+  for (b = bbuf; b < bbuf + NBFSIZE; ++b){
+    if (b->dev == dev && b->blockno == blockno) {
       b->refcnt++;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+      b->ticks = ticks;
+      release(&bcache[key].lock);
+      acquiresleep(&b->lock); // acquire sleep lock to read/write
+      return b; // return a locked buffer
     }
   }
 
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+  // Not cached. Least recently used by using ticks in trap.
+  struct buf *found = 0;
+  uint min_ticks = ~0U;
+  for (b = bbuf; b < bbuf + NBFSIZE; ++b) {
+    if (b->ticks < min_ticks && b->refcnt == 0) {
+      found = b;
+      min_ticks = b->ticks;
     }
   }
+
+  // The hints said:
+  // "When replacing a block, you might move a struct buf from one bucket to another bucket, 
+  // because the new block hashes to a different bucket. 
+  // You might have a tricky case: the new block might hash to the same bucket as the old block. 
+  // Make sure you avoid deadlock in that case."
+  // But in this implementation, it is not possible, because we search the buf inside the bucket
+
+  if (found) { // found a buffer cache
+    found->dev = dev;
+    found->blockno = blockno;
+    found->valid = 0;
+    found->refcnt = 1;
+    found->ticks = ticks;
+    release(&bcache[key].lock);
+    acquiresleep(&found->lock);
+    return found;
+  }
+
   panic("bget: no buffers");
 }
 
@@ -95,7 +115,7 @@ bread(uint dev, uint blockno)
   struct buf *b;
 
   b = bget(dev, blockno);
-  if(!b->valid) {
+  if(!b->valid) { // read buffer from the disk
     virtio_disk_rw(b, 0);
     b->valid = 1;
   }
@@ -113,6 +133,7 @@ bwrite(struct buf *b)
 
 // Release a locked buffer.
 // Move to the head of the most-recently-used list.
+// need to get the corresponding bucket number
 void
 brelse(struct buf *b)
 {
@@ -121,33 +142,27 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  acquire(&bcache[b->bucketno].lock);
   b->refcnt--;
   if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->ticks = ticks;
   }
-  
-  release(&bcache.lock);
+  release(&bcache[b->bucketno].lock);
 }
 
+// pin the buffer to the block
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  acquire(&bcache[b->bucketno].lock);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bcache[b->bucketno].lock);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  acquire(&bcache[b->bucketno].lock);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bcache[b->bucketno].lock);
 }
 
 
--- kernel/buf.h
+++ kernel/buf.h
@@ -3,10 +3,17 @@ struct buf {
   int disk;    // does disk "own" buf?
   uint dev;
   uint blockno;
-  struct sleeplock lock;
+  // spinlock is situable for multiple cores that not contend too much
+  // or the thread holds the lock for a short time
+  // if the lock contends too much or holds a long time
+  // don't spin, just give up the cpu to other threads/process (although with context switch cost) 
+  struct sleeplock lock; 
   uint refcnt;
   struct buf *prev; // LRU cache list
   struct buf *next;
   uchar data[BSIZE];
+
+  uint ticks; // using ticks time_stamp instead of list
+  int bucketno; // bucket number
 };
 
